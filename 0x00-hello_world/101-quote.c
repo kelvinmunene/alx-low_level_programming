0b815337a18b0db184013e72a47db93bcd2133d5
kelvinmunene <klvinmunene@gmail.com>
2022-09-09 01:22:36 -0700

    tired" "

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - Event point
- *
- * Return: Always 1 (Success)
+ * main - Entry point
+ * Return:(zero)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59\n);
-	return (1);
-}		
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, ch, sizeof(ch));
+return (1);
+}
