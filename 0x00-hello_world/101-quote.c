19ee2d303d233b559ac5d1b73d1e33d98bd14b0e
kelvinmunene <klvinmunene@gmail.com>
2022-09-09 11:38:32 +0300

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return(zero)
+ * Return:zero
  */
 int main(void)
 {
-char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19",\n";
+char ch[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, ch, sizeof(ch));
 return (1);
 }
