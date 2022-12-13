27e7b9b19279e66987700f4394930f89b3df86ef
kelvinmunene <klvinmunene@gmail.com>
2022-12-14 00:38:47 +0300

    update

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -1,11 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-int printf(const char *format, ...)
+int add(int a, int b)
 {
-    // This will be used to print text
-    write(1, "9 8 10 24 75 - 9\n", 17);
-    write(1, "Congratulations, you win the Jackpot!\n", 38);
-    exit(EXIT_SUCCESS);
+	return (a + b);
+}
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+int mul(int a, int b)
+{
+	return(a * b);
+}
+
+int div(int a, int b)
+{
+	if (b != 0)
+		return (a / b);
+	return (0);
+}
+
+int mod(int a, int b)
+{
+	if (b != 0)
+		return (a % b);
+	return (0);
 }
